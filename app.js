4eb322151059fc1b2a7e7c412e80e3780940f475
Cristian Vargas <cristianvargas@Cristians-MacBook-Pro.local>
2022-12-20 17:04:35 -0500

    logger added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var cookieParser = require('cookie-parser');
-
+var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,7 +12,7 @@ var app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+app.use(logger('dev'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
